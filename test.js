b37d109da59762d2b025f239a78732bcc06fb5df
rnfltpgus <jpp901220@gmail.com>
2022-01-18 16:55:23 +0900

    일단 바코집중

--- test.js
+++ test.js
@@ -1,3 +1,27 @@
-const arr = [1, 2, 3];
+// const arr = [1, 2, 3];
 
-arr.forEach(alert);
+// arr.forEach(alert);
+
+
+// Q: 아래 예제를 실행한 결과는 어떻게 되며, 왜 그런 결과가 나올까요?
+//    깊이 고민해본 후, 주석처리된 부분을 해제하고 결과를 확인해보세요.
+//
+// for (var i = 1; i < 6; i++) {
+//   setTimeout(function timer() {
+//     // console.log(i);
+//   }, i * 1000);
+// }
+
+// Q: 아래 예제를 실행한 결과는 어떻게 되며, 왜 그런 결과가 나올까요?
+//    깊이 고민해본 후, 주석처리된 부분을 해제하고 결과를 확인해보세요.
+//
+// function setTimer(j) {
+//   setTimeout(function timer() {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+// for (var i = 1; i < 6; i++) {
+//   setTimer(i);
+// }
+
